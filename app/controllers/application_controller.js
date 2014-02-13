45a06ed381f4e48606fa229fdf397be525778bce
girish <girish@girish-TravelMate-4740.(none)>
2014-02-14 00:29:05 +0530

    Initial changes

---
+++ app/controllers/application_controller.js
@@ -0,0 +1,3 @@
+before('protect from forgery', function () {
+  protectFromForgery('122584ed4479b0b31deeac7a4ddeeb211958c44c');
+});
---
+++ config/database.js
@@ -0,0 +1,17 @@
+module.exports = 
+  { "development":
+    { "driver":  "mysql"  
+    , "host" : "127.0.0.1"  
+    , "username": "root"  
+    , "password": "girish"  
+    , "database": "compound_test"  
+    }  
+  ,  
+  "production":  
+    { "driver":  "mysql"  
+    , "host" : "127.0.0.1"  
+    , "username": "root"  
+    , "password": "girish"  
+    , "database": "compound_test"  
+    }  
+  };  
